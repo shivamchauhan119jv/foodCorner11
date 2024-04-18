bbcd96235690ef1d7287d7c61f2de3ff38d1a66a
shivam <you@example.com>
2024-04-19 00:20:41 +0530

    changes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+import path from 'path';
+import { HomePageComponent } from './home-page/home-page.component';
+import { ContactPageComponent } from './contact-page/contact-page.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '', component: HomePageComponent
+    },
+    {
+        path: 'contact-page', component: ContactPageComponent
+    },
+];
